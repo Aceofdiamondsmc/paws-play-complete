89baaf280ece6ec8b17ad941010881a9d2379e02
Aceofdiamondsmc <96272967+Aceofdiamondsmc@users.noreply.github.com>
2026-03-15 00:18:35 -0700

    Add .env to ignore list

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pawsplayrepeat.app',
+  appName: 'Paws Play Repeat',
+  webDir: 'dist'
+};
+
+export default config;
