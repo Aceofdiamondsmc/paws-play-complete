28842f1c928e6408083dd9ade270cab9e374ade3
Aceofdiamondsmc <96272967+Aceofdiamondsmc@users.noreply.github.com>
2026-03-17 20:43:14 -0700

    Add server configuration for iOS and Android schemes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.pawsplayrepeat.app',
   appName: 'Paws Play Repeat',
   webDir: 'dist',
+  server: {
+    iosScheme: 'com.pawsplayrepeat.app', // Matches your actual Bundle ID
+    androidScheme: 'https'
+  },
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
